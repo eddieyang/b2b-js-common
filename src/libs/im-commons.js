bd2ee975550715e1c82cbafcc0fec8480022ad9b
Eddie.Yang <eddie.y@tc-gaming.co>
2016-12-28 12:29:27 +0800

    test

--- src/libs/im-commons.js
+++ src/libs/im-commons.js
@@ -1,5 +1,5 @@
-import SockJS from './bower-sockjs-client/sockjs';
-import './stomp-websocket';
+import SockJS from 'bower-sockjs-client';
+import 'stomp-websocket';
 
 const websocket_debug = false;
 
